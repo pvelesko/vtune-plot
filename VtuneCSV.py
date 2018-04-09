a2e49eddbce467b9704e175afed6cdd3ca5f814f
Paulius Velesko <pvelesko@gmail.com>
2018-04-09 20:26:39 +0000

    VtuneCSV to include main()

--- VtuneCSV.py
+++ VtuneCSV.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
 import pandas as pd
 import os
 class VtuneCSV():
     """
         Pass a list of strings containing paths to CSV files.
-        Each file name should only contain integers
-            - These files will be plotted in ascending order based on filename
+        If each file name contains only integers
+            - These files will be aranged in ascending order based on filename
+        else:
+            - These files will be aranged in the order they're passed in through CLI
     """
     data = None
     names = None
@@ -15,12 +18,13 @@ class VtuneCSV():
             csv_list = [csv_list]
 
         for csv_file in csv_list:
-            raw_data = pd.read_csv(csv_file)
+            raw_data = pd.read_csv(csv_file, error_bad_lines=False)
             raw_data = self.remove_empty_cols(raw_data)
             raw_data = raw_data.dropna(axis=1, how="all")
             function_col = raw_data.columns[0]
             raw_data = raw_data.set_index(function_col)
-            raw_data = raw_data.drop(' [Unknown stack frame(s)]')
+            if (' [Unknown stack frame(s)]') in raw_data:
+                raw_data = raw_data.drop(' [Unknown stack frame(s)]')
             raw_data = raw_data.rename(lambda x: x.strip(" []").replace("Loop at line ", ""))
             raw_data = raw_data.groupby(raw_data.index, sort=False).first()
             self.data.append(raw_data)
@@ -34,11 +38,35 @@ class VtuneCSV():
         a= pd.DataFrame()
         for td in self.data:
             a = pd.concat([a, td[metric]])
-        a.columns = [metric]
+
         a = a.loc[function]
-        a.index = self.names
-        a.index = [int(idx) for idx in list(a.index)]
-        a = a.sort_index(ascending=True)
-        a.index = [str(idx) for idx in list(a.index)]
+        if len(self.data) > 1:
+            a.columns = [metric]
+            a.index = self.names
+            try:
+                a.index = [int(idx) for idx in list(a.index)]
+                a = a.sort_index(ascending=True)
+                a.index = [str(idx) for idx in list(a.index)]
+            except ValueError:
+                a.index = self.names
         a.index.name = function
         return a
+
+if __name__ == "__main__":
+    import sys
+    from IPython import embed
+    assert(len(sys.argv) > 1)
+    csv = VtuneCSV(sys.argv[1:])
+    index_list = list(csv.data[0].index)
+    column_list = list(csv.data[0].columns)
+    print("************************************************")
+    print("Possibe Loops/Functions\n")
+    print("************************************************")
+    for a in index_list:
+        print(a)
+    print("************************************************")
+    print("Possibe Metrics")
+    print("************************************************")
+    for a in column_list:
+        print(a)
+    embed()
